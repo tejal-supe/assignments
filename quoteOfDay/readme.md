e66bebb60e4b18f64c4c28d5f8901c8a2e85eafb
tejal-supe <supetejalc@gmail.com>
2025-03-20 18:51:47 +0530

    Css, download quote using html2canvas, random image for the background from array

---
+++ quoteOfDay/readme.md
@@ -0,0 +1,23 @@
+# Quote of the Day
+Website that fetches and displays a random quote from the API.
+
+## Application Features : 
+ 1) Fetch data from the api and render the data
+ 2) Displays the quote and author on the page.
+ 3) User can get random quotes
+ 4) Resposive UI
+ 5) User can copy the Quote to the clipboard
+ 6) User can tweet the quote
+ 7) Random Background image for the Quote.
+
+## Tech Stack : 
+- HTML
+- CSS
+- JS
+- html2canvas (for downloading the quote)
+
+## Screenshots :
+
+
+## Deployment Url 
+### Live Url - https://quote-of-the-day-gray.vercel.app/
--- quoteOfDay/script.js
+++ quoteOfDay/script.js
@@ -2,6 +2,12 @@ const quoteDiv = document.getElementById("qoute-for-day");
 const quoteAuthor = document.getElementById("qoute-author");
 const quoteContainer = document.getElementById("quote-container");
 
+// to get random image
+const images = [
+  "https://www.w3schools.com/w3css/img_lights.jpg",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHnF-9MqUDzNEnx6gVmFOatlpY_TdXJ_CReA&s",
+];
+
 const fetchQuote = async () => {
   const url = "https://api.freeapi.app/api/v1/public/quotes/quote/random";
   const options = { method: "GET", headers: { accept: "application/json" } };
@@ -36,11 +42,20 @@ const shareOnTwitter = () => {
 
 const displayQuoteToHtml = (data) => {
   quoteDiv.innerText = data.content;
-  quoteAuthor.innerText = data.author;
+    quoteAuthor.innerText = ` - ${data.author}`;
 };
 
 const getRandomImage = async () => {
-  
+     const randomImage = images[Math.floor(Math.random() * images.length)];
+     quoteContainer.style.backgroundImage = `url(${randomImage})`;
 };
 
+const downloadQuote = () => {
+    html2canvas(quoteContainer).then((canvas) => {
+       const link = document.createElement("a");
+       link.download = "quote.png";
+       link.href = canvas.toDataURL();
+       link.click();
+     })
+}
 fetchQuote();
